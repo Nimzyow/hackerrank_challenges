bc197f41c850f99f7c4500481f2d08eb3e8b9090
Nimzyow <n_soufiani@hotmail.com>
2020-07-03 16:38:12 +0100

    diagonal difference challenge done

--- problems.js
+++ problems.js
@@ -18,11 +18,33 @@ const aVeryBigSum = (arr) => {
 };
 
 const diagonalDifference = (arr) => {
-  const matrixSize = arr[0];
-  let position = 1;
+  const matrixSize = arr[0].length;
+  let arrJoined = [].concat.apply([], arr);
+  let count = 0;
+  let position = 0;
   let firstDiagonal = 0;
   let secondDiagonal = 0;
-  return 15;
+
+  while (count < matrixSize) {
+    firstDiagonal = firstDiagonal + arrJoined[position];
+    position = position + (matrixSize + 1);
+    count++;
+  }
+  count = 0;
+  position = arrJoined.length - 1 - (matrixSize - 1);
+  while (count < matrixSize) {
+    secondDiagonal = secondDiagonal + arrJoined[position];
+    position = position - (matrixSize - 1);
+    count++;
+  }
+
+  let difference = firstDiagonal - secondDiagonal;
+
+  if (difference < 0) {
+    return difference * -1;
+  }
+
+  return difference;
 };
 
 module.exports = { compareTriplets, aVeryBigSum, diagonalDifference };
--- problems.test.js
+++ problems.test.js
@@ -33,15 +33,26 @@ describe("a very big sum", () => {
 });
 describe("Diagonal difference", () => {
   it("should return 15", () => {
-    const result = diagonalDifference([3, 11, 2, 4, 4, 5, 6, 10, 8, -12]);
+    const result = diagonalDifference([
+      [11, 2, 4],
+      [4, 5, 6],
+      [10, 8, -12],
+    ]);
     expect(result).toEqual(15);
   });
-  it.skip("should return 15", () => {
-    const result = diagonalDifference([3, 15, 4, 2, 1, 5, 8, 2, 2, 14]);
+  it("should return 25", () => {
+    const result = diagonalDifference([
+      [15, 4, 2],
+      [1, 5, 8],
+      [2, 2, 14],
+    ]);
     expect(result).toEqual(25);
   });
   it("should return 5", () => {
-    const result = diagonalDifference([2, 6, 2, 4, 5]);
+    const result = diagonalDifference([
+      [6, 2],
+      [4, 5],
+    ]);
     expect(result).toEqual(5);
   });
 });
