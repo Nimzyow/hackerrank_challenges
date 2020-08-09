fcbed293d4576958e07120e2f07e2cc2dbcaadbe
Nimzyow <n_soufiani@hotmail.com>
2020-08-09 10:06:15 +0100

    completed using sets

--- problems/inflightEntSys/InflightEntSys.js
+++ problems/inflightEntSys/InflightEntSys.js
@@ -1,24 +1,40 @@
-module.exports = (flightTime, filmTimes) => {
-  let filmTimesIndex = 0;
-  let filmToCompareTo = 0;
+// module.exports = (flightTime, filmTimes) => {
+//   let filmTimesIndex = 0;
+//   let filmToCompareTo = 0;
 
-  while (
-    filmTimesIndex < filmTimes.length &&
-    filmToCompareTo < filmTimes.length
-  ) {
-    let timeLeft = flightTime - filmTimes[filmToCompareTo];
-    const filmFinishableInFlightTime = timeLeft - filmTimes[filmTimesIndex];
-    if (filmToCompareTo === filmTimesIndex) {
-      filmTimesIndex++;
-    } else if (filmFinishableInFlightTime === 0) {
+//   while (
+//     filmTimesIndex < filmTimes.length &&
+//     filmToCompareTo < filmTimes.length
+//   ) {
+//     let timeLeft = flightTime - filmTimes[filmToCompareTo];
+//     const filmFinishableInFlightTime = timeLeft - filmTimes[filmTimesIndex];
+//     if (filmToCompareTo === filmTimesIndex) {
+//       filmTimesIndex++;
+//     } else if (filmFinishableInFlightTime === 0) {
+//       return true;
+//     } else {
+//       filmTimesIndex++;
+//     }
+//     if (filmTimesIndex === filmTimes.length) {
+//       filmToCompareTo++;
+//       filmTimesIndex = 0;
+//     }
+//   }
+//   return false;
+// };
+
+//second solution costing O(n) time and O(n) space
+
+module.exports = (flightTime, movieTimes) => {
+  let moviesSeen = new Set();
+
+  for (let i = 0; i < movieTimes.length; i++) {
+    const firstMovie = movieTimes[i];
+    const secondMovieSeen = flightTime - firstMovie;
+    if (moviesSeen.has(secondMovieSeen)) {
       return true;
-    } else {
-      filmTimesIndex++;
     }
-    if (filmTimesIndex === filmTimes.length) {
-      filmToCompareTo++;
-      filmTimesIndex = 0;
-    }
+    moviesSeen.add(firstMovie);
   }
   return false;
 };
--- problems/inflightEntSys/InflightEntSys.test.js
+++ problems/inflightEntSys/InflightEntSys.test.js
@@ -20,7 +20,7 @@ describe("inflight Entertainment System", () => {
     });
     describe("should return true when flight time is", () => {
       it("60 minutes and two films of 30 minutes are passed through", () => {
-        const result = inflightEntSys(60, [30, 30]);
+        const result = inflightEntSys(60, [20, 40]);
         expect(result).toEqual(true);
       });
       it("90 minutes and 2 films of 30 minutes and 1 film of 60 minutes are passed through", () => {
@@ -28,7 +28,11 @@ describe("inflight Entertainment System", () => {
         expect(result).toEqual(true);
       });
       it("200 minutes and 3 films of 30 minutes, 1 film of 60 minutes and 1 film of 140 minutes are passed through", () => {
-        const result = inflightEntSys(200, [30, 30, 30, 60, 140]);
+        const result = inflightEntSys(200, [30, 40, 50, 60, 140]);
+        expect(result).toEqual(true);
+      });
+      it("5 minutes and 1 film of 4 another of 3 and another of 2", () => {
+        const result = inflightEntSys(5, [4, 3, 2]);
         expect(result).toEqual(true);
       });
     });
