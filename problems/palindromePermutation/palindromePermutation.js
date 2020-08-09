7bdd47d033e64a1259a4f68923c38147cb81b020
Nimzyow <n_soufiani@hotmail.com>
2020-08-09 11:21:42 +0100

    added interviewcake solution

--- problems/palindromePermutation/palindromePermutation.js
+++ problems/palindromePermutation/palindromePermutation.js
@@ -1,14 +1,29 @@
+// module.exports = (str) => {
+//   let palindrome = new Set();
+//   for (let i = 0; i < str.length; i++) {
+//     palindrome.add(str[i]);
+//   }
+//   const palindromeSetLength = palindrome.size;
+//   const successfullPalindromeLength = str.length / 2;
+//   if (str.length % 2 === 1) {
+//     return palindromeSetLength === Math.round(successfullPalindromeLength)
+//       ? true
+//       : false;
+//   }
+//   return palindromeSetLength === successfullPalindromeLength ? true : false;
+// };
+
+//interview cake solution
+
 module.exports = (str) => {
   let palindrome = new Set();
-  for (let i = 0; i < str.length; i++) {
-    palindrome.add(str[i]);
+
+  for (const char of str) {
+    if (palindrome.has(char)) {
+      palindrome.delete(char);
+    } else {
+      palindrome.add(char);
+    }
   }
-  const palindromeSetLength = palindrome.size;
-  const successfullPalindromeLength = str.length / 2;
-  if (str.length % 2 === 1) {
-    return palindromeSetLength === Math.round(successfullPalindromeLength)
-      ? true
-      : false;
-  }
-  return palindromeSetLength === successfullPalindromeLength ? true : false;
+  return palindrome.size <= 1;
 };
