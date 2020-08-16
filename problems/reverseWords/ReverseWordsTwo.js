048201850c1fd5ea67bb36bd639a61f3a6adeae0
Nimzyow <n_soufiani@hotmail.com>
2020-08-16 01:36:15 +0100

    test completed

--- problems/reverseWords/ReverseWordsTwo.js
+++ problems/reverseWords/ReverseWordsTwo.js
@@ -3,18 +3,18 @@ module.exports = (message) => {
   let firstIndex = 0;
   let lastIndex = message.length - 1;
   reverseCharacters(message, firstIndex, lastIndex);
-  //   for (let i = 0; i < message.length; i++) {
-  //     if (message[i] === " ") {
-  //       lastIndex = i - 1;
-  //       reverseCharacters(message, firstIndex, lastIndex);
-  //       firstIndex = i + 1;
-  //     }
-  //   }
-  console.log(message);
+  for (let i = 0; i <= message.length; i++) {
+    if (message[i] === " " || i === message.length) {
+      lastIndex = i - 1;
+      reverseCharacters(message, firstIndex, lastIndex);
+      firstIndex = i + 1;
+    }
+  }
+  return message;
 };
 
 const reverseCharacters = (message, firstIndex, lastIndex) => {
-  const loopTimes = lastIndex;
+  const loopTimes = lastIndex - firstIndex;
   for (let i = 0; i < loopTimes / 2; i++) {
     const tempFirstChar = message[firstIndex];
     message[firstIndex] = message[lastIndex];
--- problems/reverseWords/ReverseWordsTwo.test.js
+++ problems/reverseWords/ReverseWordsTwo.test.js
@@ -15,7 +15,7 @@ describe("ReverseWordsTwo", () => {
       "l",
       "o",
     ]);
-    expect(result).toBe([
+    expect(result).toEqual([
       "h",
       "e",
       "l",
@@ -27,6 +27,42 @@ describe("ReverseWordsTwo", () => {
       "r",
       "l",
       "d",
+    ]);
+  });
+  it("return an array of welcome to hell", () => {
+    const result = ReverseWordsTwo([
+      "h",
+      "e",
+      "l",
+      "l",
+      " ",
+      "t ",
+      "o",
+      " ",
+      "w",
+      "e",
+      "l",
+      "c",
+      "o",
+      "m",
+      "e",
+    ]);
+    expect(result).toEqual([
+      "w",
+      "e",
+      "l",
+      "c",
+      "o",
+      "m",
+      "e",
+      " ",
+      "t ",
+      "o",
+      " ",
+      "h",
+      "e",
+      "l",
+      "l",
     ]);
   });
 });
