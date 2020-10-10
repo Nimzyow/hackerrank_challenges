0c3427c09a768ca1392e79b4124a1b6a8b6aff82
Nimzyow <n_soufiani@hotmail.com>
2020-10-10 16:12:33 +0100

    created second test

--- problems/worldCloud/WorldCloud.test.js
+++ problems/worldCloud/WorldCloud.test.js
@@ -10,4 +10,31 @@ describe("World Cloud", () => {
     ]);
     expect(result).toEqual(expected);
   });
+  it("should return for I like cake but I also like chocolate ", () => {
+    const result = WorldCloud("I like cake but I also like chocolate");
+    const expected = new Map([
+      ["I", 2],
+      ["like", 2],
+      ["cake", 1],
+      ["but", 1],
+      ["also", 1],
+      ["chocolate", 1],
+    ]);
+    expect(result).toEqual(expected);
+  });
+  it("should return for Cliff sat on the edge of a cliff", () => {
+    const result = WorldCloud(
+      "The waiter grabbed the bill to hand to the customer"
+    );
+    const expected = new Map([
+      ["the", 3],
+      ["waiter", 1],
+      ["grabbed", 1],
+      ["bill", 1],
+      ["to", 2],
+      ["hand", 1],
+      ["customer", 1],
+    ]);
+    expect(result).toEqual(expected);
+  });
 });
