9a4ce1d672a51ac0840fb8fd483fd57b4b49cfcf
Nimzyow <n_soufiani@hotmail.com>
2020-10-10 13:52:48 +0100

    passed first test of world cloud

--- problems/worldCloud/WorldCloud.js
+++ problems/worldCloud/WorldCloud.js
@@ -1,17 +1,48 @@
 module.exports = (str) => {
   //first I want to check to split these words using a helper function
-};
+  const words = splitWord(str);
 
-const isLetter = (str) => {
-  for (let i = 0; i < str.length; i++) {
-    return (
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(char) >= 0
-    );
+  const wordsToCount = new Map();
+
+  const addWordsToCount = (word) => {
+    if (wordsToCount.has(word)) {
+      wordsToCount.set(word, wordsToCount.get(word) + 1);
+    } else {
+      wordsToCount.set(word, 1);
+    }
+  };
+
+  for (let i = 0; i < words.length; i++) {
+    addWordsToCount(words[i]);
   }
-
-  return wordsSplit;
+  return wordsToCount;
 };
 
-const splitWord = (str, firstIndex, lastIndex) => {
-  return str.slice(firstIndex, lastIndex);
+const isLetter = (letter) => {
+  return (
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(letter) >= 0
+  );
+};
+
+const splitWord = (inputStr) => {
+  const words = [];
+  let currentWordStartIndex = 0;
+  let currentWordLength = 0;
+  for (let i = 0; i <= inputStr.length; i++) {
+    const character = inputStr[i];
+    if (isLetter(character)) {
+      if (currentWordLength === 0) {
+        currentWordStartIndex = i;
+      }
+      currentWordLength += 1;
+    } else {
+      const word = inputStr.slice(
+        currentWordStartIndex,
+        currentWordStartIndex + currentWordLength
+      );
+      words.push(word);
+      currentWordLength = 0;
+    }
+  }
+  return words;
 };
--- problems/worldCloud/WorldCloud.test.js
+++ problems/worldCloud/WorldCloud.test.js
@@ -1,12 +1,13 @@
 const WorldCloud = require("./WorldCloud");
 
 describe("World Cloud", () => {
-    it("should return for I like cake ", () => {
-        const result = WorldCloud("I like cake");
-        expect(result).toEqual([
-            ["I", 1],
-            ["like", 1],
-            ["cake", 1],
-        ]);
-    });
+  it("should return for I like cake ", () => {
+    const result = WorldCloud("I like cake");
+    const expected = new Map([
+      ["I", 1],
+      ["like", 1],
+      ["cake", 1],
+    ]);
+    expect(result).toEqual(expected);
+  });
 });
